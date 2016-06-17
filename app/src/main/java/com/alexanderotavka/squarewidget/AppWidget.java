6b3bff0ff9e75883fa66414984c0135039cf0e8a
Zander Otavka <zotavka@gmail.com>
2016-06-16 21:39:07 -0700

    Tweak method and attribute order

--- app/src/main/java/com/alexanderotavka/squarewidget/AppWidget.java
+++ app/src/main/java/com/alexanderotavka/squarewidget/AppWidget.java
@@ -21,8 +21,8 @@ public class AppWidget extends AppWidgetProvider {
     public static final int HOUR_NOON = 12;
     public static final int HOUR_EVENING = 18;
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+    public static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                       int appWidgetId) {
         Calendar now = Calendar.getInstance();
         int hour = now.get(Calendar.HOUR_OF_DAY);
         CharSequence greeting;
@@ -62,14 +62,6 @@ public class AppWidget extends AppWidgetProvider {
         alarmManager.set(AlarmManager.RTC_WAKEUP, soonestBreakpoint, pendingIntent);
     }
 
-    @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
-        }
-    }
-
     private static long _getDateMillis(long nowMS, int hourOfDay) {
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
@@ -89,6 +81,14 @@ public class AppWidget extends AppWidgetProvider {
         }
 
         return millis;
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
     }
 
 }
