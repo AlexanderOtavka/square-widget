4d65e39ee36bfeeff8c066e6543f1c62f3684083
Zander Otavka <zotavka@gmail.com>
2016-06-18 09:45:42 -0700

    Prevent double scheduling of events

--- app/src/main/java/com/alexanderotavka/squarewidget/AppWidget.java
+++ app/src/main/java/com/alexanderotavka/squarewidget/AppWidget.java
@@ -6,9 +6,14 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
 import android.widget.RemoteViews;
 
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Manages widget and schedules updates.
@@ -17,9 +22,11 @@ public class AppWidget extends AppWidgetProvider {
 
     public static final String INTENT_EXTRA_ID = "id";
 
-    public static final int HOUR_MIDNIGHT = 0;
-    public static final int HOUR_NOON = 12;
-    public static final int HOUR_EVENING = 18;
+    private static final String PENDING_INTENTS = "pendingIntents";
+
+    private static final int HOUR_MIDNIGHT = 0;
+    private static final int HOUR_NOON = 12;
+    private static final int HOUR_EVENING = 18;
 
     public static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                        int appWidgetId) {
@@ -46,20 +53,35 @@ public class AppWidget extends AppWidgetProvider {
         _scheduleNextUpdate(context, appWidgetId);
     }
 
+    public static boolean getAlreadyScheduled(Context context, int id) {
+        return context.getSharedPreferences(PENDING_INTENTS, Context.MODE_PRIVATE)
+                .getBoolean(String.valueOf(id), false);
+    }
+
+    public static void setAlreadyScheduled(Context context, int id, boolean alreadyScheduled) {
+        context.getSharedPreferences(PENDING_INTENTS, Context.MODE_PRIVATE).edit()
+                .putBoolean(String.valueOf(id), alreadyScheduled)
+                .apply();
+    }
+
     private static void _scheduleNextUpdate(Context context, int id) {
-        AlarmManager alarmManager =
-                (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        Intent intent = new Intent(context, AlarmManagerBroadcastReceiver.class);
-        intent.putExtra(INTENT_EXTRA_ID, id);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+        if (!getAlreadyScheduled(context, id)) {
+            setAlreadyScheduled(context, id, true);
 
-        long nowMS = System.currentTimeMillis();
-        long midnightMS = _getDateMillis(nowMS, HOUR_MIDNIGHT);
-        long noonMS = _getDateMillis(nowMS, HOUR_NOON);
-        long eveningMS = _getDateMillis(nowMS, HOUR_EVENING);
+            AlarmManager alarmManager =
+                    (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            Intent intent = new Intent(context, WidgetUpdateBroadcastReceiver.class);
+            intent.putExtra(INTENT_EXTRA_ID, id);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
 
-        long soonestBreakpoint = Math.min(midnightMS, Math.min(noonMS, eveningMS));
-        alarmManager.set(AlarmManager.RTC_WAKEUP, soonestBreakpoint, pendingIntent);
+            long nowMS = System.currentTimeMillis();
+            long midnightMS = _getDateMillis(nowMS, HOUR_MIDNIGHT);
+            long noonMS = _getDateMillis(nowMS, HOUR_NOON);
+            long eveningMS = _getDateMillis(nowMS, HOUR_EVENING);
+
+            long soonestBreakpoint = Math.min(midnightMS, Math.min(noonMS, eveningMS));
+            alarmManager.set(AlarmManager.RTC, soonestBreakpoint, pendingIntent);
+        }
     }
 
     private static long _getDateMillis(long nowMS, int hourOfDay) {
@@ -85,10 +107,22 @@ public class AppWidget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        Log.d("widget", "onUpdate: updating...");
         // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+        for (int id : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, id);
         }
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        SharedPreferences.Editor editor =
+                context.getSharedPreferences(PENDING_INTENTS, Context.MODE_PRIVATE).edit();
+        for (int id : appWidgetIds) {
+            editor.remove(String.valueOf(id));
+        }
+
+        editor.apply();
     }
 
 }
--- app/src/main/java/com/alexanderotavka/squarewidget/WidgetUpdateBroadcastReceiver.java
+++ app/src/main/java/com/alexanderotavka/squarewidget/WidgetUpdateBroadcastReceiver.java
@@ -4,21 +4,25 @@ import android.appwidget.AppWidgetManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 /**
  * Handle updating the greeting at scheduled times.
  *
  * Created by Zander on 6/16/16.
  */
-public class AlarmManagerBroadcastReceiver extends BroadcastReceiver {
+public class WidgetUpdateBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.d("widget", "onReceive: received broadcast");
+
+        AppWidgetManager manager = AppWidgetManager.getInstance(context);
         int id = intent.getIntExtra(AppWidget.INTENT_EXTRA_ID, -1);
-        if (id != -1) {
-            AppWidgetManager manager = AppWidgetManager.getInstance(context);
-            AppWidget.updateAppWidget(context, manager, id);
-        }
+        if (BuildConfig.DEBUG && id != -1) throw new AssertionError();
+
+        AppWidget.setAlreadyScheduled(context, id, false);
+        AppWidget.updateAppWidget(context, manager, id);
     }
 
 }
